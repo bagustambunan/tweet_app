6de71f5dd2f3db6f1923cb7d226e1022759ead1c
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-06-19 01:54:20 +0700

    Progate Course Ruby on Rails 5 - Study 3: Post detail, new post

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # get 'posts/index'
   get "posts/index", to: "posts#index"
+  get "posts/new", to: "posts#new"
+  get "posts/:id", to: "posts#show"
+
+  post "posts/create", to: "posts#create"
 end
