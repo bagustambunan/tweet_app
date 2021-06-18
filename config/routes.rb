42c28be6d4c3b0a89b21d1032c69a2a742436fd6
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-06-18 14:40:01 +0700

    Progate Course Ruby on Rails 5 - Study 2: Database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   # get 'home/top'
   get "/", to: "home#top"
   get "about", to: "home#about"
+
+  # get 'posts/index'
+  get "posts/index", to: "posts#index"
 end
