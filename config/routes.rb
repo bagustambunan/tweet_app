da47da0b87f70b161c5910025d4fc1993b4e6ea9
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-06-23 22:55:50 +0700

    Progate Course Ruby on Rails 5 - Study 6: User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+
+  get "signup", to:"users#new"
+  post "users/create", to:"users#create"
+  get "users/:id/edit", to:"users#edit"
+  post "users/:id/update", to:"users#update"
   
-  # get 'home/top'
+  get 'users/index'
+  get "users/:id", to:"users#show"
+
   get "/", to: "home#top"
   get "about", to: "home#about"
 
-  # get 'posts/index'
   get "posts/index", to: "posts#index"
   get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
   get "posts/:id/edit", to: "posts#edit"
-
   post "posts/create", to: "posts#create"
   post "posts/:id/update", to: "posts#update"
   post "posts/:id/destroy", to: "posts#destroy"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_221633) do
+ActiveRecord::Schema.define(version: 2021_06_23_101130) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
