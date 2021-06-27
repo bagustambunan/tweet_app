44fda3d07d4bd45c453fdb3573d102b910bccd24
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-06-27 21:52:30 +0700

    Progate Course Ruby on Rails 5 - Study 9: Associating Posts with the User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_151814) do
+ActiveRecord::Schema.define(version: 2021_06_27_130303) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
