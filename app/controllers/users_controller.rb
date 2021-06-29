d45086bf5214afa4408cf91ee68f14b35c7cabe8
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-06-29 17:07:19 +0700

    Progate Course Ruby on Rails 5 - Study 11: Encrypting Passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user, {only: [:index,:show,:edit,:update]}
-  before_action :forbid_login_user, {only:[:new,:create,:login_form,:login]}
-  before_action :ensure_correct_user, {only: [:edit,:update]}
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   
   def index
     @users = User.all
@@ -54,16 +54,13 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
-
+  
   def login_form
-    
   end
-
+  
   def login
-    @user = User.find_by(email: params[:email],
-                        password: params[:password])
-
-    if @user
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "You have logged in successfully"
       redirect_to("/posts/index")
@@ -73,20 +70,19 @@ class UsersController < ApplicationController
       @password = params[:password]
       render("users/login_form")
     end
-
   end
-
+  
   def logout
     session[:user_id] = nil
     flash[:notice] = "You have logged out successfully"
     redirect_to("/login")
   end
-
+  
   def likes
     @user = User.find_by(id: params[:id])
     @likes = Like.where(user_id: @user.id)
   end
-
+  
   def ensure_correct_user
     if @current_user.id != params[:id].to_i
       flash[:notice] = "Unauthorized access"
